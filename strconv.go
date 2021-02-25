6f48e26e22d8fb8fb60f8ce3846d10af60927c2f
anatole <anatole.thien@ynov.com>
2021-02-25 02:16:47 +0100

    atoi addition

--- strconv.go
+++ strconv.go
@@ -1,10 +1,33 @@
 package myfunc
 
-import "strconv"
+import (
+	"errors"
+)
 
-func Atoi(s string) int {
+func Atoi(s string) (int, error) {
 
-strconv.Atoi()
+	var f = s[0]
+	var n int
 
-	return 0
+	if f == '-' || f == '+' {
+		s = s[1:]
+		if len(s) < 1 {
+			return 0, errors.New("invalid string")
+		}
+
+	}
+
+	for _, v := range s {
+		v -= '0'
+		if v > 9 {
+			return 0, errors.New("invalid string")
+		}
+		n = n*10 + int(v)
+	}
+
+	if f == '-' {
+		n = -n
+	}
+
+	return n, nil
 }
